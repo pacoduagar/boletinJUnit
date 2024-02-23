f702187cf8c8dcaeba7f30ba3d2745fd5ff15d7e
pacoduagar <150923972+pacoduagar@users.noreply.github.com>
2024-02-23 10:38:42 +0100

    Update PilaTest.java

--- boletinJUnit/tests/boletinJUnit/PilaTest.java
+++ boletinJUnit/tests/boletinJUnit/PilaTest.java
@@ -14,58 +14,58 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class PilaTest {
+public class StackTest {
 	
-	Pila pila;
+	Pila stack;
 	int n = 10;
 	
 	@BeforeEach
 	void init() {
-		pila = new Pila();
+		stack = new Pila();
 	}
 	
 	@AfterEach
 	void finish() {
-		pila = null;
+		stack = null;
 	}
 	
 	@ParameterizedTest
 	@ValueSource(ints = {0, 2, 10, 20, 23})
 	void testAdd(int num) {
-		pila.push(num);
+		stack.push(num);
 	}
 	
 	@Test
 	void testRemoveFromEmptyPile() {
-		assertThat(pila.pop(), nullValue());
+		assertThat(stack.pop(), nullValue());
 	}
 	
 	@Test
 	void testRemoveFromNonEmptyList() {
-		pila.push(n);
-		assertThat(pila.pop(), is(n));
+		stack.push(n);
+		assertThat(stack.pop(), is(n));
 	}
 	
 	@Test
 	void testEmptyList() {
-		assertTrue(pila.isEmpty());
+		assertTrue(stack.isEmpty());
 	}
 	
 	@Test
 	void testNonEmptyList() {
-		pila.push(n);
-		assertFalse(pila.isEmpty());
+		stack.push(n);
+		assertFalse(stack.isEmpty());
 	}
 	
 	@Test
 	void testLastOneFromEmptyList() {
-		assertThat(pila.top(), nullValue());
+		assertThat(stack.top(), nullValue());
 	}
 	
 	@Test
 	void testLastOneFromNonEmptyList() {
-		pila.push(n);
-		assertThat(pila.top(), is(n));
+		stack.push(n);
+		assertThat(stack.top(), is(n));
 	}
 
 }
